6111e6877a4ba47e7833c3f993208b6dca081903
Chirag Patel <patelc75@yahoo.com>
2015-06-14 16:56:12 -0400

    new specials file and updated schedule ruby file

--- coop_scheduler.rb
+++ coop_scheduler.rb
@@ -11,22 +11,23 @@ require 'json'
 require 'chronic'
 
 
-cal = Google::Calendar.new(:client_id     => "419624150549-7plpq38mughrvbnt3jde6vf5urge64ga.apps.googleusercontent.com",
+cal_bumblebee = Google::Calendar.new(
+                           :client_id     => "419624150549-7plpq38mughrvbnt3jde6vf5urge64ga.apps.googleusercontent.com",
                            :client_secret => "4TJFhxJ1QrS8Ev8jM57DYemb",
                            :calendar      => "7dib4m37gjfmi971t952fh6cac@group.calendar.google.com",
                            :redirect_url  => "urn:ietf:wg:oauth:2.0:oob" # this is what Google uses for 'applications'
                            )
 
 refresh_token = "1/-JJFnXmK-2Wyb0ImBpXwvaapSIf_JQ89OfSW8ARO5wU"
-cal.login_with_refresh_token(refresh_token)
+cal_bumblebee.login_with_refresh_token(refresh_token)
 monday_gcal = Chronic.parse "last monday 8am" #=> 2015-06-08 09:00:00 -0400
 friday_gcal = monday_gcal + (5*24*60*60)
-events = cal.find_events_in_range(monday_gcal, friday_gcal, :expand_recurring_events => true)
+events = cal_bumblebee.find_events_in_range(monday_gcal, friday_gcal, :expand_recurring_events => true)
 puts "TOTAL EVENTS: #{events.count} from #{monday_gcal.to_s} to #{friday_gcal.to_s}"
-#puts cal.events
+#puts cal_bumblebee.events
 
 # Query events
-#cal.find_events('your search string')
+#cal_bumblebee.find_events('your search string')
 
 specials_file = File.read('specials.json')
 periods_file = File.read('periods.json')
@@ -51,11 +52,9 @@ specials.each do |special|
     end
   end
 end
-breakpoint 
 
 
-
-# event = cal.create_event do |e|
+# event = cal_bumblebee.create_event do |e|
 #   e.title = 'A Cool Event'
 #   e.start_time = Time.now
 #   e.end_time = Time.now + (60 * 60) # seconds * min
@@ -63,7 +62,7 @@ breakpoint
 
 # puts event
 
-# event = cal.find_or_create_event_by_id(event.id) do |e|
+# event = cal_bumblebee.find_or_create_event_by_id(event.id) do |e|
 #   e.title = 'An Updated Cool Event'
 #   e.end_time = Time.now + (60 * 60 * 2) # seconds * min * hours
 #   e.color_id = 3  # google allows colors 0-11
@@ -71,7 +70,7 @@ breakpoint
 
 
 
-def prompt_for_refresh_token
+def prompt_for_refresh_token(cal)
 =begin
   puts "Do you already have a refresh token? (y/n)"
   has_token = $stdin.gets.chomp
